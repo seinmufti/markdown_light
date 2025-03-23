61fd0399465b5b8ecf4c724cfc42d735762c6dcc
Sein <hosein.mufti@gmail.com>
2025-03-24 01:59:48 +0300

    bug found, second item doesnt get added right after item 1 in output.

--- list_brain.py
+++ list_brain.py
@@ -9,58 +9,61 @@ def main():
     print(''.join(output_list))
 
 
-def main_environment_simulator(content, nesting_flag=False):
+def main_environment_simulator(content, is_nesting_flag=False):
     output_list = []
 
     ul_open = False
     content_lines = content.splitlines()
     content_lines_len = len(content_lines)
-    last_line = False
+    is_last_line = False
 
     for i, line in enumerate(content_lines):
         if i ==  content_lines_len - 1:
-            last_line = True
+            is_last_line = True
 
-        li_line, ul_open, nesting_flag = convert_list(line, ul_open, last_line, nesting_flag)
+        li_line, ul_open, is_nesting_flag = convert_list(line, ul_open, is_last_line, is_nesting_flag)
         output_list += li_line
+
+        if not is_last_line:
+            output_list.append('\n')
     
     return output_list
 
 
-def convert_list(line, ul_open, last_line, nesting_flag):
+def convert_list(line, ul_open, is_last_line, is_nesting_flag):
     output = []
-    list_pattern = r'-\s(.*)'
+    list_pattern = r'(?:\s\s)*-\s(.*)'
     match = re.match(list_pattern, line)
     if match:
+        match = match.group(1)
         # open ul if not already open
         if not ul_open:
-            if nesting_flag:
+            if is_nesting_flag:
                 output.append('\n')
 
             output.extend(["<ul>", '\n'])
             ul_open = True
 
-        # line_text = match.group(1)
-        line_text = main_environment_simulator(match.group(1), True)
+        line_text = main_environment_simulator(match, True)
         output.append("<li>")
         output += line_text
-        output.extend(["</li>", '\n'])
+        output.extend(["</li>"])
     else:
         if ul_open:
-            output.append("</ul>")
+            output.extend(["</ul>", '\n'])
             ul_open = False
 
         # Remove leading and trailing white spaces
         line = line.strip()
         output.append(line)
 
-    if last_line and ul_open:
-        output.append("</ul>")
-        if nesting_flag:
+    if is_last_line and ul_open:
+        output.extend(['\n', "</ul>"])
+        if is_nesting_flag:
             output.append('\n')
         ul_open = False
 
-    return output, ul_open, nesting_flag
+    return output, ul_open, is_nesting_flag
 
 
 if __name__ == "__main__":
